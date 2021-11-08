d607a5583eea548c36e14a7dc7c8807d5b7c9629
Muir-W <63028131+Muir-W@users.noreply.github.com>
2021-11-07 20:34:40 -0600

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -10,7 +10,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello From Point Venture"
-   response.end('Hello From Point Venture\n')
+   response.end('Hello World - CloudFormation template generated website\n')
 }).listen(3333)
 
 // Console will print the message
